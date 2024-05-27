1790544c63d3aba5b9f051d217f00c216ec9707e
Abhijit Kadalli <abhijitkadalli@nitgoa.ac.in>
2024-05-27 19:38:13 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,17 +1,21 @@
 import json
 import sys
 import bencodepy
-# import bencodepy - available if you need it!
-# import requests - available if you need it!
+
 
 bc = bencodepy.Bencode(encoding='utf-8')
-# Examples:
-#
-# - decode_bencode(b"5:hello") -> b"hello"
-# - decode_bencode(b"10:hello12345") -> b"hello12345"
+
 
 def decode_bencode(bencoded_value):
     return bc.decode(bencoded_value)
+
+def info_torrent(location):
+    with open(location, 'rb') as f:
+        data = f.read()
+    decoded = bc.decode(data)
+    tracker_url = decoded[b'announce']
+    size_file = decoded[b'info'][b'length']
+    return tracker_url, size_file
         
 def main():
     command = sys.argv[1]
@@ -30,6 +34,13 @@ def main():
             raise TypeError(f"Type not serializable: {type(data)}")
 
         print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
+    elif command == "info":
+        location = sys.argv[2]
+
+        tracker_url, size_file = info_torrent(location)
+        print(f"Tracker URL: {tracker_url.decode()}")
+        print(f"Size of file: {size_file}")
+
     else:
         raise NotImplementedError(f"Unknown command {command}")
 
