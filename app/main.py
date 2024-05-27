a9178da011116e4a922eb7651ec0ee7b168ca54b
Abhijit Kadalli <abhijitkadalli@nitgoa.ac.in>
2024-05-27 12:37:36 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -14,8 +14,10 @@ def decode_bencode(bencoded_value):
         if first_colon_index == -1:
             raise ValueError("Invalid encoded value")
         return bencoded_value[first_colon_index+1:]
-    else:
-        raise NotImplementedError("Only strings are supported at the moment")
+    elif chr(bencoded_value[0]) == "i" and chr(bencoded_value[-1]) == "e":
+        # return the integer value
+        return int(bencoded_value[1:-1])
+        
 
 
 def main():
